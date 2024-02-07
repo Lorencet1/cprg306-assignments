b919404558e5c5baed9dbb346f0f32d4e4392b81
Lorencet1 <Lorence.talay@edu.sait.ca>
2024-02-06 21:08:38 -0700

    Your commit message

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 
 import Link from "next/link";
+import NewItem from "./week-4/new-item";
+import React from "react";
 
 export default function Page() {
   return (
@@ -10,6 +12,9 @@ export default function Page() {
       </Link>
       <Link href="/week-3">
         <h2 className='text-2xl pt-10'>Week 3 Assignment</h2>
+      </Link>
+      <Link href="/week-4">
+        <h2 className='text-2xl pt-10'>Week 4 Assignment</h2>
       </Link>
     </main>
   );
